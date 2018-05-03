45c3c7254c2810f01dc1f3758a7262184f527177
Ajayi Samuel <ajayisamuelofficial@gmail.com>
2018-05-03 17:19:28 +0100

    updated database

--- database/vendordb.sql
+++ database/vendordb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 12, 2018 at 03:46 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Host: localhost
+-- Generation Time: May 03, 2018 at 06:11 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.1.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -53,34 +53,31 @@ CREATE TABLE `vendor_info` (
   `vendor_username` varchar(50) NOT NULL,
   `vendor_email` varchar(100) NOT NULL,
   `vendor_password` varchar(50) NOT NULL,
-  `company_name` varchar(50) NOT NULL,
-  `phone_number` varchar(15) NOT NULL,
-  `company_description` text NOT NULL,
-  `facebook_link` varchar(255) NOT NULL,
-  `instagram_link` varchar(255) NOT NULL,
-  `logo` varchar(255) NOT NULL,
-  `image1` varchar(255) NOT NULL,
-  `image2` varchar(255) NOT NULL,
-  `image3` varchar(255) NOT NULL,
-  `category` varchar(30) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `date` varchar(50) NOT NULL
+  `company_name` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `company_description` text,
+  `long_company_description` text,
+  `facebook_link` varchar(255) DEFAULT NULL,
+  `instagram_link` varchar(255) DEFAULT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `background_img` varchar(255) DEFAULT NULL,
+  `image1` varchar(255) DEFAULT NULL,
+  `image2` varchar(255) DEFAULT NULL,
+  `image3` varchar(255) DEFAULT NULL,
+  `category` varchar(30) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `vendor_info`
 --
 
-INSERT INTO `vendor_info` (`id`, `vendor_username`, `vendor_email`, `vendor_password`, `company_name`, `phone_number`, `company_description`, `facebook_link`, `instagram_link`, `logo`, `image1`, `image2`, `image3`, `category`, `address`, `date`) VALUES
-(1, 'AjayiSamuel', 'jasperunit18@yahoo.com', 'p', '', '', '', '', '', '', '', '', '', '', '', ''),
-(2, 'AjayiSamuel2', 'jasperunit18@yahoo.com2', 'pp', '', '', '', '', '', '', '', '', '', '', '', ''),
-(3, 'AA', 'jasperunit18@yahoo.com3', 'p', 'mdfnl', 'mdnvm,x', '.kdfmnl', '.,msfdlk', '.,mcfl', 'Abstract HD Wappapers Downloadha (1).jpg', 'Abstract HD Wappapers Downloadha (1).jpg', 'Abstract HD Wappapers Downloadha (6).jpg', 'Abstract HD Wappapers Downloadha (6).jpg', 'chocolate', '', '19-02-2018'),
-(4, 'JAY', 'jay@gmail.com', 'pass', ',jkdrhuk', ',jgnjl', 'ljkcfgk', 'lkfhlf', 'lktjgld', '', '', '', 'No Thrid image selected', 'cake', '', '19-02-2018'),
-(5, 'samuelajayi', 'email@gmail.com', 'money', 'kscmioa', '74735628', 'sjvnjs', 'sk', 'df', 'logo/5a8c2e20a96de5.58461601.jpg', '', '', '', 'cake', '', '20-02-2018'),
-(6, 'hope', 'hopeman@gmial.com', 'help', 'work test', '1234567890', 'test', 'fb', 'ig', '', 'images/5a8c34e2020280.49653604.jpg', 'images/5a8c34e21fb899.65054393.jpg', 'images/5a8c34e22c9090.18661003.jpg', 'cake', '', '20-02-2018'),
-(7, 'don', 'don@gmail.com', 'password', 'company', 'number', 'description', 'fb', 'ig', '', 'images/5a8c41f41231c6.35761382.jpg', 'images/5a8c41f42c2b82.49261848.jpg', 'images/5a8c41f4450065.73095776.jpg', 'small chops', '', '20-02-2018'),
-(8, 'ASA', 'asa@gmail.com', 'password', 'ASA', '1234', 'ASA D', 'fb', 'ig', 'logo/5a8c4d503ee548.76090582.jpg', 'images/5a8c4d504aa680.84426375.jpg', 'images/5a8c4d505b36e7.06856731.jpg', 'images/5a8c4d5077a753.34178538.jpg', 'edible arrangement', '', '20-02-2018'),
-(9, 'ASA2', 'asa2@gmail.com', 'asapassword', 'thecompany', '1234567890', 'the best in the world', 'fblink', 'ig', '../logofolder/Abstract HD Wappapers Downloadha (2).jpg', 'images/5a8d7c7dc89387.98147873.jpg', 'images/5a8d8532afeb13.45729289.jpg', 'images/5a8d8532d4aee7.32034063.jpg', 'cake', '', '21-02-2018');
+INSERT INTO `vendor_info` (`id`, `vendor_username`, `vendor_email`, `vendor_password`, `company_name`, `phone_number`, `company_description`, `long_company_description`, `facebook_link`, `instagram_link`, `logo`, `background_img`, `image1`, `image2`, `image3`, `category`, `address`, `date`) VALUES
+(6, 'hope', 'hopeman@gmial.com', 'help', 'work test', '1234567890', 'test', NULL, 'fb', 'ig', '', NULL, 'images/5a8c34e2020280.49653604.jpg', 'images/5a8c34e21fb899.65054393.jpg', 'images/5a8c34e22c9090.18661003.jpg', 'cake', '', '20-02-2018'),
+(7, 'don', 'don@gmail.com', 'password', 'company', 'number', 'description', NULL, 'fb', 'ig', '', NULL, 'images/5a8c41f41231c6.35761382.jpg', 'images/5a8c41f42c2b82.49261848.jpg', 'images/5a8c41f4450065.73095776.jpg', 'small chops', '', '20-02-2018'),
+(8, 'ASA', 'asa@gmail.com', 'password', 'ASA', '1234', 'ASA D', NULL, 'fb', 'ig', 'logo/5a8c4d503ee548.76090582.jpg', NULL, 'images/5a8c4d504aa680.84426375.jpg', 'images/5a8c4d505b36e7.06856731.jpg', 'images/5a8c4d5077a753.34178538.jpg', 'edible arrangement', '', '20-02-2018'),
+(9, 'ASA2', 'asa2@gmail.com', 'asapassword', 'thecompany', '1234567890', 'the best in the world', NULL, 'fblink', 'ig', '../logofolder/Abstract HD Wappapers Downloadha (2).jpg', NULL, 'images/5a8d7c7dc89387.98147873.jpg', 'images/5a8d8532afeb13.45729289.jpg', 'images/5a8d8532d4aee7.32034063.jpg', 'cake', '', '21-02-2018');
 
 --
 -- Indexes for dumped tables
